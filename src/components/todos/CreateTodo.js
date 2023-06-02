fa232e35b7af84364a75ad76dcc169a60f634f4f
Janan Akkad <jananakkad@hotmail.co.uk>
2023-06-02 15:57:49 +0100

    complete

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -12,10 +12,14 @@ class CreateTodo extends Component {
     });
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-  }
+  
+handleSubmit = event => {
+  event.preventDefault();
+  this.props.addTodo(this.state)
+  this.setState({
+    text: "",
+  })
+}
   
   render() {
     return(
